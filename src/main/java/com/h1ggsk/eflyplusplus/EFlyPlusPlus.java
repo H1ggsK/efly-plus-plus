65b3795b92a7fe3f6a659f4f6a657aa712aa9e25
H1ggsK <theohavel@proton.me>
2025-03-10 20:36:20 -0700

    Added src

---
+++ src/main/java/com/h1ggsk/eflyplusplus/EFlyPlusPlus.java
@@ -0,0 +1,37 @@
+package com.h1ggsk.eflyplusplus;
+
+import com.h1ggsk.eflyplusplus.modules.ElytraFlyPlusPlus;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class EFlyPlusPlus extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("H1ggsK_ Addons");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing EFlyPlusPlus Meteor Addon");
+
+        // Modules
+        Modules.get().add(new ElytraFlyPlusPlus());
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.h1ggsk.eflyplusplus";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("H1ggsK", "efly-plus-plus");
+    }
+}
---
+++ src/main/java/com/h1ggsk/eflyplusplus/H1ggskModule.java
@@ -0,0 +1,38 @@
+package com.h1ggsk.eflyplusplus;
+
+import meteordevelopment.meteorclient.mixininterface.IChatHud;
+import meteordevelopment.meteorclient.systems.config.Config;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+/**
+ * @author OLEPOSSU
+ * @author h1ggsk
+ */
+
+public class H1ggskModule extends Module {
+    private final String prefix = Formatting.WHITE + "[" + Formatting.DARK_PURPLE + "Addons" + Formatting.WHITE + "]";
+    public final int priority;
+
+    public H1ggskModule(Category category, String name, String description) {
+        super(category, name, description);
+        this.priority = 100;
+    }
+
+    //  Messages
+    public void sendToggledMsg() {
+        if (Config.get().chatFeedback.get() && chatFeedback && mc.world != null) {
+            ChatUtils.forceNextPrefixClass(getClass());
+            String msg = prefix + " " + Formatting.WHITE + name + (isActive() ? Formatting.GREEN + " ON" : Formatting.RED + " OFF");
+            sendMessage(Text.of(msg), hashCode());
+        }
+    }
+
+    public void sendMessage(Text text, int id) {
+        ((IChatHud) mc.inGameHud.getChatHud()).meteor$add(text, id);
+    }
+
+}
